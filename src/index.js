1f198aa6e8af19e6dfdd072dee3bfaafa292e7a9
LucsT <lucas@cozycloud.cc>
2018-05-28 18:11:59 +0200

    Pdf is well reconised, enhance filename

--- src/index.js
+++ src/index.js
@@ -119,7 +119,8 @@ async function parseDocuments($) {
     order.filename =
       order.date.format('YYYY-MM-DD') +
       '_' +
-      order.amount.toString().replace('.', '€') +
+      order.amount +
+      '€' +
       '.pdf'
     order.date = order.date.toDate()
     const url = new URL(order.fileurl, baseUrl)
